58509306e6c4d4848af615b57203dd5d70bcfb85
le yang <thomas.yang.v@gmail.com>
2020-01-18 18:03:34 +0100

    add two project one for client, one for Server

---
+++ GameClient/client/src/client/Client.java
@@ -0,0 +1,30 @@
+package client;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+
+   public static void main(String [] args) {
+      String serverName = args[0];
+      int port = Integer.parseInt(args[1]);
+      while(true) {
+	      try {
+	         System.out.println("Connecting to " + serverName + " on port " + port);
+	         Socket client = new Socket(serverName, port);
+	         
+	         System.out.println("Just connected to " + client.getRemoteSocketAddress());
+	         OutputStream outToServer = client.getOutputStream();
+	         DataOutputStream out = new DataOutputStream(outToServer);
+	         
+	         out.writeUTF("Hello from " + client.getLocalSocketAddress());
+	         InputStream inFromServer = client.getInputStream();
+	         DataInputStream in = new DataInputStream(inFromServer);
+	         
+	         System.out.println("Server says " + in.readUTF());
+	         out.writeUTF("new message will come!");
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      }
+      }
+   }
+}
