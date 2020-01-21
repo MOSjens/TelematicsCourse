357f1df20419e751ba1fe0f5e5af70c2eeb3a931
David <ujeyh@student.kit.edu>
2020-01-21 10:51:02 +0100

    added parsing of general text message and test

--- GameServer/src/main/java/parser/SendParser.java
+++ GameServer/src/main/java/parser/SendParser.java
@@ -11,7 +11,9 @@ import messages.*;
  *
  */
 public class SendParser {
-	private byte[] header = new byte[7];
+	private byte[] header;
+	private byte[] payload;
+	private byte[] byteArray;
 	private int length;
 	private byte[] payloadLength;
 
@@ -19,22 +21,81 @@ public class SendParser {
 		
 	}
 	
-	public byte[] parse(Message sendMessage) {
+	public byte[] messageToByteArray(Message sendMessage) {
+		header = new byte[7];
+		payload = new byte[0];
 		header [0]= (byte) sendMessage.getVersion();
 		header [1]= (byte) sendMessage.getGroup();
 		header [2] = (byte) sendMessage.getType();
 		length = 0;
 		
+		switch(sendMessage.getMessageType()) {
+		case ANSWER:
+			break;
+		case ANSWER_RESULT:
+			break;
+		case BUZZ:
+			break;
+		case BUZZ_RESULT:
+			break;
+		case CATEGORY_SELECTION:
+			break;
+		case CATEGORY_SELECTOR_ANNOUNCEMENT:
+			break;
+		case GAME_END:
+			break;
+		case GENERAL_TEXT:
+			payload = generalTextToByteArray(sendMessage);
+			break;
+		case PLAYER_LIST:
+			break;
+		case PLAYER_READY:
+			break;
+		case QUESTION:
+			break;
+		case SCOREBOARD:
+			break;
+		case SCREW:
+			break;
+		case SCREW_RESULT:
+			break;
+		case SIGN_ON:
+			break;
+		case SIGN_ON_RESPONSE:
+			break;
+		default:
+			break;
+		
+		}
+		length = payload.length;
 		payloadLength = intToByteArray(length);
 		for(int i = 0; i < 4; i++) {
 			header[i+3] = payloadLength[i];
 		}
-		return header;
+		
+		byteArray = new byte[payload.length+header.length];
+		int j =0;
+		for(int i = 0; i < payload.length+header.length; i++) {
+			if(i < header.length) {
+				byteArray [i] = header[i];
+			}
+			else {
+				byteArray [i] = payload[j];
+				j++;
+			}	
+		}
+		return byteArray;
 		
 	}
 	
 	
 	
+	private byte[] generalTextToByteArray(Message sendMessage) {
+		GeneralText generalText = (GeneralText) sendMessage;
+		byte[] payload = generalText.getGeneralText().getBytes();
+		return payload;
+	}
+
 	public static byte[] intToByteArray(int i) {
 	    final ByteBuffer bb = ByteBuffer.allocate(Integer.SIZE / Byte.SIZE);
 	    bb.putInt(i);
--- GameServer/src/test/java/parser/TestSendParser.java
+++ GameServer/src/test/java/parser/TestSendParser.java
@@ -19,13 +19,24 @@ public class TestSendParser {
 	public void testParse() {
 		SignOn signOn = new SignOn();
 		signOn.setPlayerAlias("🦄🐿️");
-		byte[]data = new byte[] {0x01,0x00,0x00,0x00,0x00,0x00,0x00
+		byte[]dataTest;
+		byte[]dataSignOn = new byte[] {0x01,0x00,0x00,0x00,0x00,0x00,0x00
 				//0x01,0x00,0x00,0x00,0x00,0x00,0x0b,(byte) 0xf0,(byte) 0x9f,(byte) 0xa6,
 				//(byte) 0x84,(byte) 0xf0,(byte) 0x9f,(byte) 0x90,(byte) 0xbf,(byte) 0xef,(byte) 0xb8,(byte) 0x8f
 		};
-
-		for(int i = 0; i < data.length; i++) {
-			assertEquals(data[i], sendParser.parse(signOn)[i]);
+		dataTest = sendParser.messageToByteArray(signOn);
+		for(int i = 0; i < dataSignOn.length; i++) {
+			assertEquals(dataSignOn[i], dataTest[i]);
+		}
+		GeneralText GeneralText = new GeneralText();
+		GeneralText.setGeneralText("🦄🐿️");
+		byte[]dataGeneralText = new byte[] {
+				0x01,0x03,0x00,0x00,0x00,0x00,0x0b,(byte) 0xf0,(byte) 0x9f,(byte) 0xa6,
+				(byte) 0x84,(byte) 0xf0,(byte) 0x9f,(byte) 0x90,(byte) 0xbf,(byte) 0xef,(byte) 0xb8,(byte) 0x8f
+		};
+		dataTest = sendParser.messageToByteArray(GeneralText);
+		for(int i = 0; i < dataGeneralText.length; i++) {
+			assertEquals(dataGeneralText[i], dataTest[i]);
 		}
 
 	}
